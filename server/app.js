2f0ddf105143505887a2a48e1ad62101cf5ce07a
ridgra <im.ridgra@gmail.com>
2020-08-17 21:04:59 +0700

    deployment prep

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
+if (process.env.NODE_ENV == 'development') {
+  require('dotenv').config();
+}
 const express = require('express');
 const cors = require('cors');
 const errHandler = require('./middlewares/errHandler');
 const router = require('./routes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use('/', router);
 app.use(errHandler);
